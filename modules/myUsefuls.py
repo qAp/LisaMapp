c8d51c3043936379840e8241a56cb32cbda8e0a7
Jack <jackchungchiehyu@gmail.com>
2012-06-30 19:36:20 -0500

    sp_linalg.eig() changed to sp_linalg.eigh() in myUsefuls.get_noise_freq_domain_CovarMatrix()
    This is a significant correction.  scipy.linalg.eig() does not does not
    output a matrix of orthonormal eigenvectors, which is required by the simulation
    method. This is necessarily done using
    scipy.linalg.eigh(), which applies to Hermitian matrices.  This is caused by
    confusion with Matlab, whose eig() automatically works on Hermitian matrices
    and produce orthnormal eigenvectors.
    
    After this change, the normalisation test carried out for the case of
    different powerlaw (1/f) coloured time-series simulated from a single
    covariance matrix is passed.  The reconstructed PSDs and CSDs,
    averaged over 365 and 25 realisations, are shown to
    agree with the analytical ones better than before.  The time and
    realisation-averaged empirical mean of the data still fluctuates by as much
    as before for freq-domain simulations, much larger than direct randn() sim
    (for the white noise case).  The same averages for the variance and the covariance
    are appear more or less the same as before.  However, for the 1-sigma test,
    the optimal estimates more or less fluctuate evenly about the true value, and 70%
    fall within 1 theoretical sigma.  This is the first time that a simulation
    with coloured time-series (generated from a single covariance matrix having
    non-zero off-diagonal elements) has worked.
    
    	modified:   modules/myUsefuls.py

--- modules/myUsefuls.py
+++ modules/myUsefuls.py
@@ -128,14 +128,14 @@ def get_noise_freq_domain_CovarMatrix( comatrix , df , inittime , parityN , seed
         C = comatrix[ :,:,k ]
         if not np.allclose( C , np.conj( np.transpose( C ) ) ) :
             print "Covariance matrix NOT Hermitian! Unphysical."        
-        w , V = sp_linalg.eig( C )
+        w , V = sp_linalg.eigh( C )
         for m in range( w.shape[0] ) :
             w[m] = np.real( w[m] )
             if np.abs(w[m]) / np.max(w) < 1e-10 :
                 w[m] = 0
             if w[m] < 0 :
                 print 'Negative eigenvalue! Simulating unpysical signal...'
-        print w
+
         ntilde_p[ :,k ] =  np.conj( np.sqrt( N / (2*stime) ) * np.dot( V , np.dot( np.sqrt( np.diag( w ) ) , zs[ :,k ] ) ) )
     
     zerofill = np.zeros( ( Nts , 1 ) )
